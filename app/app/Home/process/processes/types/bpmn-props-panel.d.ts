d31603f66147233bc3ad631d30d31e69aeb5572f
Ali Razi <95105244+alirazi1992@users.noreply.github.com>
2025-08-17 15:58:14 +0330

    Update bpmn-props-panel.d.ts

--- app/app/Home/process/processes/types/bpmn-props-panel.d.ts
+++ app/app/Home/process/processes/types/bpmn-props-panel.d.ts
@@ -1,3 +1,21 @@
 declare module '@bpmn-io/properties-panel';
 declare module 'bpmn-js-properties-panel';
 
+// types/bpmn-shims.d.ts
+
+declare module "diagram-js/lib/features/align";
+declare module "diagram-js/lib/features/distribute";
+declare module "diagram-js/lib/navigation/zoomscroll";
+declare module "diagram-js/lib/navigation/movecanvas";
+declare module "diagram-js/lib/features/keyboard";
+declare module "diagram-js/lib/features/bendpoints";
+declare module "diagram-js/lib/features/create";
+declare module "diagram-js/lib/features/space-tool";
+declare module "diagram-js/lib/features/lasso-tool";
+declare module "diagram-js/lib/features/hand-tool";
+
+// If you ever need it elsewhere, keep this:
+declare module "bpmn-moddle";
+
+
+declare module "bpmn-js-properties-panel/lib/provider/bpmn";
